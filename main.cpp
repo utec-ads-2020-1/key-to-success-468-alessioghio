2d0ce28ad36cb2158a91d17b2cccc9f99ee0b537
Alessio <alessiogdanz@gmail.com>
2020-04-08 12:43:06 -0500

    Code works with udebug inputs

--- main.cpp
+++ main.cpp
@@ -2,47 +2,65 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
-#include <map>
+#include <vector>
 using namespace std;
 
-void countAndMap(string input, map<char, int>& myMap);
+void countAndMap(string input, vector<pair<char, int>>& myMap);
 
 int main(){
-    string coded, decoded;
+    string coded, decoded, temp;
     stringstream ss;
     int iter;
-    map<char, int> chCntCoded;
-    map<char, int> chCntDecoded;
-    while (getline(cin, coded)){
-        ss.str(coded);
-        ss >> iter;
-        ss.clear();
-
-        getline(cin, coded); // Ignore blank line
-        for (int i = 0; i < iter; i++){
-            getline(cin, coded);
-            sort(coded.begin(), coded.end());
-            countAndMap(coded, chCntCoded);            
-            getline(cin, decoded);
-            countAndMap(decoded, chCntDecoded);
-            if(i!=iter-1){
-                getline(cin, coded); // ignore blank line
+    vector<pair<char, int>> chCntCoded, chCntDecoded;
+    getline(cin, coded);
+    ss.str(coded);
+    ss >> iter;
+    ss.clear();
+    getline(cin, coded); // Ignore blank line
+    
+    for (int i = 0; i < iter; i++){
+        getline(cin, coded);
+        sort(coded.begin(), coded.end());
+        countAndMap(coded, chCntCoded);
+        sort(chCntCoded.begin(), chCntCoded.end(),
+            [](pair<char, int> a, pair<char, int> b){ return (a.second < b.second);});
+        getline(cin, decoded);
+        temp = decoded;
+        sort(temp.begin(), temp.end());
+        countAndMap(temp, chCntDecoded);
+        sort(chCntDecoded.begin(), chCntDecoded.end(),
+            [](pair<char, int> a, pair<char, int> b){ return (a.second < b.second);});
+        // Decode
+        for(char& c : decoded){
+            for (int idx = 0; idx < chCntDecoded.size(); idx++){
+                if(c == chCntDecoded[idx].first){
+                    c = chCntCoded[idx].first;
+                    break;
+                }
             }
+            cout << c;
+        };
+        if(i!=iter-1){
+            cout << endl << endl;
+            getline(cin, coded); // ignore blank line
         }
-        
+        chCntCoded.clear();
+        chCntDecoded.clear();
     }
     
     return EXIT_SUCCESS;
 }
 
-void countAndMap(string input, map<char, int>& myMap){
+void countAndMap(string input, vector<pair<char, int>>& myMap){
     char ch = ' ';
+    int idx = -1;
     for (int i = 0; i < input.size(); i++){
         if(ch!=input[i]){
+            idx++;
             ch = input[i];
-            myMap.insert(make_pair(ch, 1));
+            myMap.push_back(make_pair(ch, 1));
         }else{
-            myMap[ch] += 1;
+            myMap[idx].second += 1;
         }
     }
 }
